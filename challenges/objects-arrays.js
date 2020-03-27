cb3130f3b4de5abcb08fa556a50c66c024b6a34d
Sean Kramer <seandominickramer@gmail.com>
2020-03-27 12:07:50 -0400

    Add forEach method to create displayNames array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -121,6 +121,10 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+
+zooAnimals.forEach (function(element){return displayNames.push(`Name:${element.animal_name}, Scientific:${element.scientific_name}.`);
+}) 
+
 console.log(displayNames);
 
 /* Request 2: .map()
