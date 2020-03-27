8b89612d0274c37628a2a3667ab1832602abaafc
Sean Kramer <seandominickramer@gmail.com>
2020-03-27 12:28:59 -0400

    Add map method to create lowCaseAnimalNames array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -133,7 +133,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(function(item){
+  return item.animal_name.toLowerCase();
+})
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
